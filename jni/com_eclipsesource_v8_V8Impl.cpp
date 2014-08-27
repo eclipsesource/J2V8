585099e149021ef6ed1579eff8fc43d6741753cc
Ian Bull <irbull@eclipsesource.com>
2014-08-27 15:00:40 -0700

    Add a main to test our build.

--- jni/com_eclipsesource_v8_V8Impl.cpp
+++ jni/com_eclipsesource_v8_V8Impl.cpp
@@ -1223,3 +1223,32 @@ void throwError( JNIEnv *env, const char *message ) {
     exClass = (env)->FindClass(className);
     (env)->ThrowNew(exClass, message );
 }
+
+int main(int argc, char* argv[]) {
+  // Create a new Isolate and make it the current one.
+  Isolate* isolate = Isolate::New();
+  Isolate::Scope isolate_scope(isolate);
+
+  // Create a stack-allocated handle scope.
+  HandleScope handle_scope(isolate);
+
+  // Create a new context.
+  Local<Context> context = Context::New(isolate);
+
+  // Enter the context for compiling and running the hello world script.
+  Context::Scope context_scope(context);
+
+  // Create a string containing the JavaScript source code.
+  Local<String> source = String::NewFromUtf8(isolate, "'Hello' + ', World!'");
+
+  // Compile the source code.
+  Local<Script> script = Script::Compile(source);
+
+  // Run the script to get the result.
+  Local<Value> result = script->Run();
+
+  // Convert the result to an UTF8 string and print it.
+  String::Utf8Value utf8(result);
+  printf("%s\n", *utf8);
+  return 0;
+}
