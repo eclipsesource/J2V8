d7a94f442119a1b1f7f41262c5850c1760a72090
Ian Bull <irbull@eclipsesource.com>
2014-08-28 10:50:11 -0700

    Replace Object->Set with Object->ForceSet
    On Linux, Set does not appear to be defined. While I invistage this I'm
    going to use ForceSet.

--- jni/com_eclipsesource_v8_V8Impl.cpp
+++ jni/com_eclipsesource_v8_V8Impl.cpp
@@ -644,7 +644,7 @@ JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1add__IILjava_lang_String_2
 
 	Local<String> v8Key = String::NewFromUtf8(isolate, env -> GetStringUTFChars(key, NULL));
 	Local<Value> v8Value = v8::Int32::New(isolate, value);
-	global->Set(v8Key,  v8Value);
+	global->ForceSet(v8Key,  v8Value);
 }
 
 JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1add__IILjava_lang_String_2D
@@ -660,7 +660,7 @@ JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1add__IILjava_lang_String_2
 
 	Local<String> v8Key = String::NewFromUtf8(isolate, env -> GetStringUTFChars(key, NULL));
 	Local<Value> v8Value = v8::Number::New(isolate, value);
-	global->Set( v8Key,  v8Value);
+	global->ForceSet( v8Key,  v8Value);
 }
 
 JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1add__IILjava_lang_String_2Ljava_lang_String_2
@@ -676,7 +676,7 @@ JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1add__IILjava_lang_String_2
 
 	Local<String> v8Key = String::NewFromUtf8(isolate, env -> GetStringUTFChars(key, NULL));
 	Local<String> v8Value = String::NewFromUtf8(isolate, env -> GetStringUTFChars(value, NULL));
-	global->Set( v8Key,  v8Value);
+	global->ForceSet( v8Key,  v8Value);
 }
 
 JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1add__IILjava_lang_String_2Z
@@ -692,7 +692,7 @@ JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1add__IILjava_lang_String_2
 
 	Local<String> v8Key = String::NewFromUtf8(isolate, env -> GetStringUTFChars(key, NULL));
 	Local<Value> v8Value = v8::Boolean::New(isolate, value);
-	global->Set( v8Key,  v8Value);
+	global->ForceSet( v8Key,  v8Value);
 }
 
 JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1addObject
@@ -708,7 +708,7 @@ JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1addObject
 
 	Local<String> v8Key = String::NewFromUtf8(isolate, env -> GetStringUTFChars(key, NULL));
 	Local<Value> v8Value = Local<Object>::New(isolate, *v8Isolates[v8RuntimeHandle]->objects[valueHandle]);
-	global->Set( v8Key,  v8Value);
+	global->ForceSet( v8Key,  v8Value);
 }
 
 JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1addArray
@@ -724,7 +724,7 @@ JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1addArray
 
 	Local<String> v8Key = String::NewFromUtf8(isolate, env -> GetStringUTFChars(key, NULL));
 	Local<Value> v8Value = Local<Object>::New(isolate, *v8Isolates[v8RuntimeHandle]->arrays[valueHandle]);
-	global->Set( v8Key,  v8Value);
+	global->ForceSet( v8Key,  v8Value);
 }
 
 
@@ -1170,7 +1170,7 @@ JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1registerJavaMethod
 	MethodDescriptor* md = new MethodDescriptor();
 	md ->  methodID = methodID;
 	md -> v8RuntimeHandle = v8RuntimeHandle;
-	global->Set(String::NewFromUtf8(isolate, utf_string), Function::New(isolate, voidCallback, External::New(isolate, md)));
+	global->ForceSet(String::NewFromUtf8(isolate, utf_string), Function::New(isolate, voidCallback, External::New(isolate, md)));
 }
 
 JNIEXPORT void JNICALL Java_com_eclipsesource_v8_V8__1setPrototype
