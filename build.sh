2701c5b3f883cae709b3dce7ba971ce7db834bbf
Ian Bull <irbull@eclipsesource.com>
2014-09-09 16:04:08 -0700

    Update the local build file for my current setup.
    build.sh is currently only used for my local build, but I would like to
    keep it in version control until we have something more portable. This
    updated build.sh for my updated paths.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-cd bin
-javah com.eclipsesource.v8.V8Impl
-cp com_eclipsesource_v8_V8Impl.h ../jni
-cd ../jni
-clang++ -stdlib=libstdc++ -Iinclude ../v8/libv8_base.x64.a ../v8/libv8_nosnapshot.x64.a ../v8/libicudata.a ../v8/libicuuc.a ../v8/libicui18n.a -I ../v8 -I /Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home/include -I /Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home/include/darwin/ -shared -o libj2v8.dylib com_eclipsesource_v8_V8Impl.cpp 
+cd target/classes
+javah com.eclipsesource.v8.V8
+cp com_eclipsesource_v8_V8.h ../../jni/com_eclipsesource_v8_V8Impl.h
+cd ../../jni
+clang++  -stdlib=libstdc++ -Iinclude ../v8_x64_working/libv8_base.x64.a ../v8_x64_working/libv8_nosnapshot.x64.a ../v8_x64_working/libicudata.a ../v8_x64_working/libicuuc.a ../v8_x64_working/libicui18n.a -I ../v8 -I /Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home/include -I /Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home/include/darwin/ -shared -o libj2v8.dylib com_eclipsesource_v8_V8Impl.cpp 
 
