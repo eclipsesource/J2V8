14e8efd632a1b73063088a22a339af9e3fb7ec58
Ian Bull <irbull@eclipsesource.com>
2014-09-10 14:54:07 -0700

    Add support for converting from Map / List to V8Object / V8Array
    This change-set adds an API for creating V8Objects / V8Arrays from Maps
    and Lists. If the Map or List contains an invalid object, an
    IllegalStateException is thrown.

--- src/main/java/com/eclipsesource/v8/utils/V8ObjectUtils.java
+++ src/main/java/com/eclipsesource/v8/utils/V8ObjectUtils.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import com.eclipsesource.v8.V8;
 import com.eclipsesource.v8.V8Array;
 import com.eclipsesource.v8.V8Object;
 
 public class V8ObjectUtils {
 
-    public static Map<String, ? super Object> asMap(final V8Object object) {
+    public static Map<String, ? super Object> toMap(final V8Object object) {
         Map<String, ? super Object> result = new HashMap<>();
         String[] keys = object.getKeys();
         for (String key : keys) {
@@ -19,12 +21,65 @@ public class V8ObjectUtils {
         return result;
     }
 
-    public static List<? super Object> asList(final V8Array array) {
+    public static List<? super Object> toList(final V8Array array) {
         List<? super Object> result = new ArrayList<>();
         for (int i = 0; i < array.length(); i++) {
             result.add(getValue(array, "" + i));
         }
         return result;
+    }
+
+    public static V8Object toV8Object(final V8 v8, final Map<String, ? extends Object> map) {
+        V8Object result = new V8Object(v8);
+        try {
+            for (Entry<String, ? extends Object> entry : map.entrySet()) {
+                setValue(v8, result, entry.getKey(), entry.getValue());
+            }
+        } catch (IllegalStateException e) {
+            result.release();
+            throw e;
+        }
+        return result;
+    }
+
+    public static V8Array toV8Array(final V8 v8, final List<? extends Object> list) {
+        V8Array result = new V8Array(v8);
+        try {
+            for (int i = 0; i < list.size(); i++) {
+                Object value = list.get(i);
+                setValue(v8, result, "" + i, value);
+            }
+        } catch (IllegalStateException e) {
+            result.release();
+            throw e;
+        }
+
+        return result;
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private static void setValue(final V8 v8, final V8Object result, final String key, final Object value) {
+        if (value == null) {
+            result.addUndefined(key);
+        } else if (value instanceof Integer) {
+            result.add(key, (int) value);
+        } else if (value instanceof Double) {
+            result.add(key, (double) value);
+        } else if (value instanceof String) {
+            result.add(key, (String) value);
+        } else if (value instanceof Boolean) {
+            result.add(key, (boolean) value);
+        } else if (value instanceof Map) {
+            V8Object object = toV8Object(v8, (Map) value);
+            result.add(key, object);
+            object.release();
+        } else if (value instanceof List) {
+            V8Array array = toV8Array(v8, (List) value);
+            result.add(key, array);
+            array.release();
+        } else {
+            throw new IllegalStateException("Unsupported Object of type: " + value.getClass());
+        }
     }
 
     private static Object getValue(final V8Object object, final String key) {
@@ -41,21 +96,21 @@ public class V8ObjectUtils {
             case V8Object.V8_ARRAY:
                 V8Array array = object.getArray(key);
                 try {
-                    return asList(array);
+                    return toList(array);
                 } finally {
                     array.release();
                 }
             case V8Object.V8_OBJECT:
                 V8Object child = object.getObject(key);
                 try {
-                    return asMap(child);
+                    return toMap(child);
                 } finally {
                     child.release();
                 }
             case V8Object.VOID:
                 return null;
             default:
-                throw new IllegalArgumentException("Cannot find type for key: " + key);
+                throw new IllegalStateException("Cannot find type for key: " + key);
         }
     }
 
--- src/test/java/com/eclipsesource/v8/utils/tests/V8ObjectUtilsTest.java
+++ src/test/java/com/eclipsesource/v8/utils/tests/V8ObjectUtilsTest.java
@@ -1,5 +1,8 @@
 package com.eclipsesource.v8.utils.tests;
 
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -41,7 +44,7 @@ public class V8ObjectUtilsTest {
     public void testCreateIntegerMapFromV8Object() {
         V8Object object = v8.executeObjectScript("x = {a:1, b:2, c:3}; x");
 
-        Map<String, Object> map = V8ObjectUtils.asMap(object);
+        Map<String, Object> map = V8ObjectUtils.toMap(object);
 
         assertEquals(3, map.size());
         assertEquals(1, map.get("a"));
@@ -54,7 +57,7 @@ public class V8ObjectUtilsTest {
     public void testCreateDoubleMapFromV8Object() {
         V8Object object = v8.executeObjectScript("x = {a:1.1, b:2.2, c:3.3, d:4.4}; x");
 
-        Map<String, Object> map = V8ObjectUtils.asMap(object);
+        Map<String, Object> map = V8ObjectUtils.toMap(object);
 
         assertEquals(4, map.size());
         assertEquals(1.1, (double) map.get("a"), 0.000001);
@@ -68,7 +71,7 @@ public class V8ObjectUtilsTest {
     public void testCreateStringMapFromV8Object() {
         V8Object object = v8.executeObjectScript("x = {a:'foo', b:'bar', c:'baz', d:'boo'}; x");
 
-        Map<String, Object> map = V8ObjectUtils.asMap(object);
+        Map<String, Object> map = V8ObjectUtils.toMap(object);
 
         assertEquals(4, map.size());
         assertEquals("foo", map.get("a"));
@@ -82,7 +85,7 @@ public class V8ObjectUtilsTest {
     public void testCreateBooleanMapFromV8Object() {
         V8Object object = v8.executeObjectScript("x = {a:true, b:1==1, c:false, d:1!=1}; x");
 
-        Map<String, Object> map = V8ObjectUtils.asMap(object);
+        Map<String, Object> map = V8ObjectUtils.toMap(object);
 
         assertEquals(4, map.size());
         assertTrue((boolean) map.get("a"));
@@ -96,7 +99,7 @@ public class V8ObjectUtilsTest {
     public void testCreateMixedMapFromV8Object() {
         V8Object object = v8.executeObjectScript("x = {boolean:true, integer:1, double:3.14159, string:'hello'}; x");
 
-        Map<String, Object> map = V8ObjectUtils.asMap(object);
+        Map<String, Object> map = V8ObjectUtils.toMap(object);
 
         assertEquals(4, map.size());
         assertTrue((boolean) map.get("boolean"));
@@ -111,7 +114,7 @@ public class V8ObjectUtilsTest {
     public void testCreateNestedMapFromV8Object() {
         V8Object object = v8.executeObjectScript("x = { name : { first :'john', last: 'smith'}, age: 7}; x");
 
-        Map<String, Object> map = V8ObjectUtils.asMap(object);
+        Map<String, Object> map = V8ObjectUtils.toMap(object);
 
         assertEquals(2, map.size());
         assertEquals(7, map.get("age"));
@@ -124,7 +127,7 @@ public class V8ObjectUtilsTest {
     public void testCreateListFromV8Array() {
         V8Array array = v8.executeArrayScript("x = [1,2,3]; x");
 
-        List<Object> list = V8ObjectUtils.asList(array);
+        List<Object> list = V8ObjectUtils.toList(array);
 
         assertEquals(3, list.size());
         assertEquals(1, list.get(0));
@@ -137,7 +140,7 @@ public class V8ObjectUtilsTest {
     public void testCreateListWithNullsFromV8Array() {
         V8Array array = v8.executeArrayScript("x = [1,2,3]; x[9] = 10; x");
 
-        List<Object> list = V8ObjectUtils.asList(array);
+        List<Object> list = V8ObjectUtils.toList(array);
 
         assertEquals(10, list.size());
         assertEquals(1, list.get(0));
@@ -158,7 +161,7 @@ public class V8ObjectUtilsTest {
     public void testCreateMatrixFromV8Array() {
         V8Array array = v8.executeArrayScript("x = [[1,2,3],[true,false,true],['this','that','other']]; x");
 
-        List<Object> list = V8ObjectUtils.asList(array);
+        List<Object> list = V8ObjectUtils.toList(array);
 
         assertEquals(3, list.size());
         assertEquals(3, ((List) list.get(0)).size());
@@ -181,7 +184,7 @@ public class V8ObjectUtilsTest {
     public void testCreateMapWithLists() {
         V8Object object = v8.executeObjectScript("x = {a:[1,2,3], b:[4,5,6]}; x;");
 
-        Map<String, Object> map = V8ObjectUtils.asMap(object);
+        Map<String, Object> map = V8ObjectUtils.toMap(object);
 
         assertEquals(2, map.size());
         assertEquals(1, ((List) map.get("a")).get(0));
@@ -191,6 +194,318 @@ public class V8ObjectUtilsTest {
         assertEquals(5, ((List) map.get("b")).get(1));
         assertEquals(6, ((List) map.get("b")).get(2));
         object.release();
+    }
+
+    @Test
+    public void testCreateV8ObjectFromStringMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("first", "john");
+        map.put("last", "smith");
+
+        int size = registerAndRelease("result", map);
+
+        assertEquals(2, size);
+        assertEquals("john", v8.executeStringScript("result.first"));
+        assertEquals("smith", v8.executeStringScript("result['last']"));
+    }
+
+    @Test
+    public void testCreateV8ObjectFromIntegerMap() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 3);
+
+        int size = registerAndRelease("result", map);
+
+        assertEquals(2, size);
+        assertEquals(1, v8.executeIntScript("result.a"));
+        assertEquals(3, v8.executeIntScript("result['b']"));
+    }
+
+    @Test
+    public void testCreateV8ObjectFromDoubleMap() {
+        Map<String, Double> map = new HashMap<>();
+        map.put("a", 1.1);
+        map.put("b", 3.14159);
+        map.put("c", 4.999);
+
+        int size = registerAndRelease("result", map);
+
+        assertEquals(3, size);
+        assertEquals(1.1, v8.executeDoubleScript("result.a"), 0.000001);
+        assertEquals(3.14159, v8.executeDoubleScript("result['b']"), 0.000001);
+        assertEquals(4.999, v8.executeDoubleScript("result['c']"), 0.000001);
+    }
+
+    @Test
+    public void testCreateV8ObjectFromBooleanMap() {
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("a", true);
+        map.put("b", true);
+        map.put("c", false);
+
+        int size = registerAndRelease("result", map);
+
+        assertEquals(3, size);
+        assertTrue(v8.executeBooleanScript("result.a"));
+        assertTrue(v8.executeBooleanScript("result['b']"));
+        assertFalse(v8.executeBooleanScript("result['c']"));
+    }
+
+    @Test
+    public void testCreatev8ObjectWithNulls() {
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("a", true);
+        map.put("b", null);
+
+        int size = registerAndRelease("result", map);
+
+        assertTrue(v8.executeBooleanScript("result.a"));
+        assertTrue(v8.executeBooleanScript("typeof result.b === 'undefined'"));
+        assertEquals(2, size);
+    }
+
+    @Test
+    public void testCreateV8ObjectFromMixedMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("boolean", true);
+        map.put("integer", 7);
+        map.put("double", 3.14159);
+        map.put("String", "hello");
+        map.put("undefined", null);
+
+        int size = registerAndRelease("result", map);
+
+        assertEquals(5, size);
+        V8Object object = v8.getObject("result");
+        assertTrue(object.getBoolean("boolean"));
+        assertEquals(7, object.getInteger("integer"));
+        assertEquals(3.14159, object.getDouble("double"), 0.000001);
+        assertEquals("hello", object.getString("String"));
+        assertEquals(V8Object.UNDEFINED, object.getType("undefined"));
+        object.release();
+    }
+
+    @Test
+    public void testCreateV8ArrayFromIntegerList() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        for (int i = 2; i < 10; i++) {
+            list.add(list.get(i - 1) + list.get(i - 2));
+        }
+
+        int size = registerAndRelease("result", list);
+
+        assertEquals(10, size);
+        assertEquals(1, v8.executeIntScript("result[0]"));
+        assertEquals(1, v8.executeIntScript("result[1]"));
+        assertEquals(2, v8.executeIntScript("result[2]"));
+        assertEquals(3, v8.executeIntScript("result[3]"));
+        assertEquals(5, v8.executeIntScript("result[4]"));
+        assertEquals(8, v8.executeIntScript("result[5]"));
+        assertEquals(13, v8.executeIntScript("result[6]"));
+        assertEquals(21, v8.executeIntScript("result[7]"));
+        assertEquals(34, v8.executeIntScript("result[8]"));
+        assertEquals(55, v8.executeIntScript("result[9]"));
+    }
+
+    @Test
+    public void testCreateV8ArrayFromDoubleList() {
+        List<Double> list = new ArrayList<>();
+        list.add(3.14159);
+        list.add(4.1);
+        list.add(5.3);
+
+        int size = registerAndRelease("result", list);
+
+        assertEquals(3, size);
+        assertEquals(3.14159, v8.executeDoubleScript("result[0]"), 0.000001);
+        assertEquals(4.1, v8.executeDoubleScript("result[1]"), 0.000001);
+        assertEquals(5.3, v8.executeDoubleScript("result[2]"), 0.000001);
+    }
+
+    @Test
+    public void testCreateV8ArrayFromBooleanList() {
+        List<Boolean> list = new ArrayList<>();
+        list.add(true);
+        list.add(false);
+
+        int size = registerAndRelease("result", list);
+
+        assertEquals(2, size);
+        assertTrue(v8.executeBooleanScript("result[0]"));
+        assertFalse(v8.executeBooleanScript("result[1]"));
+    }
+
+    @Test
+    public void testCreateV8ArrayFromStringList() {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("world");
+
+        int size = registerAndRelease("result", list);
+
+        assertEquals(2, size);
+        assertEquals("hello", v8.executeStringScript("result[0]"));
+        assertEquals("world", v8.executeStringScript("result[1]"));
+    }
+
+    @Test
+    public void testCreateV8ArrayWithNullValues() {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add(null);
+        list.add("world");
+
+        int size = registerAndRelease("result", list);
+
+        assertEquals(3, size);
+        assertEquals("hello", v8.executeStringScript("result[0]"));
+        assertTrue(v8.executeBooleanScript("typeof result[1] === 'undefined'"));
+        assertEquals("world", v8.executeStringScript("result[2]"));
+    }
+
+    @Test
+    public void testCreateV8AraryFromMixedList() {
+        List<Object> list = new ArrayList<>();
+        list.add("string");
+        list.add(7);
+        list.add(3.14159);
+        list.add(true);
+        list.add(null);
+
+        int size = registerAndRelease("result", list);
+
+        assertEquals(5, size);
+        assertEquals("string", v8.executeStringScript("result[0]"));
+        assertEquals(7, v8.executeIntScript("result[1]"));
+        assertEquals(3.14159, v8.executeDoubleScript("result[2]"), 0.000001);
+        assertTrue(v8.executeBooleanScript("result[3]"));
+        assertTrue(v8.executeBooleanScript("typeof result[4] === 'undefined'"));
+    }
+
+    @Test
+    public void testCreateV8ArrayOfMaps() {
+        List<Map<String, Integer>> list = new ArrayList<>();
+        Map<String, Integer> m1 = new HashMap<>();
+        m1.put("Sadie", 7);
+        m1.put("Lily", 5);
+        m1.put("Maggie", 3);
+        Map<String, Integer> m2 = new HashMap<>();
+        m2.put("Ian", 38);
+        list.add(m1);
+        list.add(m2);
+
+        int size = registerAndRelease("result", list);
+
+        assertEquals(2, size);
+        assertEquals(7, v8.executeIntScript("result[0].Sadie"));
+        assertEquals(5, v8.executeIntScript("result[0].Lily"));
+        assertEquals(3, v8.executeIntScript("result[0].Maggie"));
+        assertEquals(38, v8.executeIntScript("result[1].Ian"));
+    }
+
+    @Test
+    public void testCreatev8ArrayFromMatrix() {
+        List<List<Integer>> matrix = new ArrayList<>();
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(1);
+        l1.add(2);
+        l1.add(3);
+        List<Integer> l2 = new ArrayList<>();
+        l2.add(4);
+        l2.add(5);
+        l2.add(6);
+        List<Integer> l3 = new ArrayList<>();
+        l3.add(7);
+        l3.add(8);
+        l3.add(9);
+        matrix.add(l1);
+        matrix.add(l2);
+        matrix.add(l3);
+
+        int size = registerAndRelease("result", matrix);
+        assertEquals(3, size);
+        assertEquals(3, v8.executeIntScript("result[0].length"));
+        assertEquals(3, v8.executeIntScript("result[1].length"));
+        assertEquals(3, v8.executeIntScript("result[2].length"));
+        assertEquals(1, v8.executeIntScript("result[0][0]"));
+        assertEquals(2, v8.executeIntScript("result[0][1]"));
+        assertEquals(3, v8.executeIntScript("result[0][2]"));
+        assertEquals(4, v8.executeIntScript("result[1][0]"));
+        assertEquals(5, v8.executeIntScript("result[1][1]"));
+        assertEquals(6, v8.executeIntScript("result[1][2]"));
+        assertEquals(7, v8.executeIntScript("result[2][0]"));
+        assertEquals(8, v8.executeIntScript("result[2][1]"));
+        assertEquals(9, v8.executeIntScript("result[2][2]"));
+    }
+
+    @Test
+    public void testCreateV8ObjectFromMapOfLists() {
+        Map<String, List<String>> map = new HashMap<>();
+        List<String> l1 = new ArrayList<>();
+        l1.add("first");
+        l1.add("second");
+        l1.add("third");
+        List<String> l2 = new ArrayList<>();
+        l2.add("a");
+        l2.add("b");
+        l2.add("c");
+        List<String> l3 = new ArrayList<>();
+        l3.add("dog");
+        l3.add("cat");
+        map.put("numbers", l1);
+        map.put("letters", l2);
+        map.put("animals", l3);
+
+        int size = registerAndRelease("result", map);
+
+        assertEquals(3, size);
+        assertEquals("first", v8.executeStringScript("result.numbers[0]"));
+        assertEquals("second", v8.executeStringScript("result.numbers[1]"));
+        assertEquals("third", v8.executeStringScript("result.numbers[2]"));
+        assertEquals("a", v8.executeStringScript("result.letters[0]"));
+        assertEquals("b", v8.executeStringScript("result.letters[1]"));
+        assertEquals("c", v8.executeStringScript("result.letters[2]"));
+        assertEquals("dog", v8.executeStringScript("result.animals[0]"));
+        assertEquals("cat", v8.executeStringScript("result.animals[1]"));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testCreateV8ObjectWithInvalidContents() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("first", new Rectangle());
+
+        registerAndRelease("result", map);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testCreateV8ArrayWithInvalidContents() {
+        List<Object> list = new ArrayList<>();
+        list.add(new Rectangle());
+
+        registerAndRelease("result", list);
+    }
+
+    private int registerAndRelease(final String name, final List<? extends Object> list) {
+        V8Array array = V8ObjectUtils.toV8Array(v8, list);
+        v8.add(name, array);
+        int size = array.length();
+        array.release();
+        return size;
+    }
+
+    private int registerAndRelease(final String name, final Map<String, ? extends Object> map) {
+        V8Object object = V8ObjectUtils.toV8Object(v8, map);
+        v8.add(name, object);
+        int size = getNumberOfProperties(object);
+        object.release();
+        return size;
+    }
+
+    private int getNumberOfProperties(final V8Object object) {
+        return object.getKeys().length;
     }
 
 }
