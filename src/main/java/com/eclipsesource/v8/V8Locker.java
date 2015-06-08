6ed9242ec7a5020606d9a802e206f3bda8a98fdc
irbull <irbull@eclipsesource.com>
2015-06-08 15:09:19 -0700

    Add JavaDoc to the public API in Locker.java.

--- src/main/java/com/eclipsesource/v8/V8Locker.java
+++ src/main/java/com/eclipsesource/v8/V8Locker.java
@@ -10,14 +10,25 @@
  ******************************************************************************/
 package com.eclipsesource.v8;
 
+/**
+ * Represents a lock for a V8Runtime that can be moved between
+ * threads. When instantiated, the lock is automatically assigned
+ * to the current thread. If another thread wishes to acquire the
+ * lock, it must first be released.
+ */
 public class V8Locker {
 
     private Thread thread = null;
 
-    public V8Locker() {
+    V8Locker() {
         acquire();
     }
 
+    /**
+     * Acquire the lock if it's currently not acquired by another
+     * thread. If it's current held by another thread, an
+     * Error will be thrown.
+     */
     public synchronized void acquire() {
         if ((thread != null) && (thread != Thread.currentThread())) {
             throw new Error("Invalid V8 thread access.");
@@ -25,17 +36,28 @@ public class V8Locker {
         thread = Thread.currentThread();
     }
 
+    /**
+     * Release the lock if it's currently held by the calling thread.
+     * If the current thread does not hold the lock, and error will be
+     * thrown.
+     */
     public synchronized void release() {
         checkThread();
         thread = null;
     }
 
-    public void checkThread() {
+    void checkThread() {
         if ((thread != Thread.currentThread())) {
             throw new Error("Invalid V8 thread access.");
         }
     }
 
+    /**
+     * Check if the current thread holds this lock.
+     *
+     * @return Returns true if the current thread holds the lock,
+     * false otherwise.
+     */
     public boolean hasLock() {
         return thread == Thread.currentThread();
     }
