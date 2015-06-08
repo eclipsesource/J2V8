86b7f0bf4ec597f47e3add68cf3633879e96dd75
irbull <irbull@eclipsesource.com>
2015-06-08 14:17:14 -0700

    Add JavaDoc to the public API in JavaCallback.

--- src/main/java/com/eclipsesource/v8/JavaVoidCallback.java
+++ src/main/java/com/eclipsesource/v8/JavaVoidCallback.java
@@ -10,8 +10,24 @@
  ******************************************************************************/
 package com.eclipsesource.v8;
 
+/**
+ * Classes that implement this interface provide a method
+ * which can be invoked from JavaScript.
+ *
+ * After creating an instance of a class that implements this
+ * interface it can be registered as a Callback on a V8Object.
+ */
 public interface JavaVoidCallback {
 
+    /**
+     * Called when a JS Function invokes a the registered Java
+     * method.
+     *
+     * @param receiver The V8Object that the function was called on.
+     * @param parameters The parameters passed to the JS Function. The
+     * parameter array does not need to be released, by any objects accessed
+     * from the array must be.
+     */
     public void invoke(V8Object receiver, V8Array parameters);
 
 }
