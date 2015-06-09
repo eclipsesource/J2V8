091a82cc12373f12705bc729cd0187226e5c2dea
irbull <irbull@eclipsesource.com>
2015-06-08 19:15:03 -0700

    Add JavaDoc to the public API in V8Thread and V8Runnable.

--- src/main/java/com/eclipsesource/v8/utils/V8Runnable.java
+++ src/main/java/com/eclipsesource/v8/utils/V8Runnable.java
@@ -12,8 +12,18 @@ package com.eclipsesource.v8.utils;
 
 import com.eclipsesource.v8.V8;
 
+/**
+ * Classes can implement this interface to execute arbitrary code on
+ * isolated V8 runtime on its own thread. Instances of classes that
+ * implement this interface can be passed to V8Thread.
+ */
 public interface V8Runnable {
 
+    /**
+     * Execute the code on the provided runtime.
+     *
+     * @param runtime The V8 runtime assigned to this runnable.
+     */
     public void run(final V8 runtime);
 
 }
--- src/main/java/com/eclipsesource/v8/utils/V8Thread.java
+++ src/main/java/com/eclipsesource/v8/utils/V8Thread.java
@@ -12,15 +12,33 @@ package com.eclipsesource.v8.utils;
 
 import com.eclipsesource.v8.V8;
 
+/**
+ * A Thread with its own V8 runtime. The thread will create a runtime,
+ * and execute a runnable on that runtime. When the thread ends,
+ * the runtime will be released.
+ *
+ * It's suggested that you *DO NOT* release the lock on the runtime.
+ * If the lock is released, you will need to ensure that the runtime
+ * is properly released.
+ */
 public class V8Thread extends Thread {
 
     private final V8Runnable target;
     private V8               runtime;
 
+    /**
+     * Create as new Thread with its own V8Runtime.
+     *
+     * @param target The code to execute with the given runtime.
+     */
     public V8Thread(final V8Runnable target) {
         this.target = target;
     }
 
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Thread#run()
+     */
     @Override
     public void run() {
         runtime = V8.createV8Runtime();
