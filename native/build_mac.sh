a9af0e039c1148405f1daedb6141b0616d821acc
ReneHollander <rene.hollander@hotmail.de>
2015-08-01 22:13:48 +0200

    fix output path of mac build

--- native/build_mac.sh
+++ native/build_mac.sh
@@ -8,9 +8,9 @@ mkdir -p lib/
 make clean
 ./configure --dest-cpu=x86
 make -j8
-cp out/Release/lib.target/libj2v8.dynlib lib/libj2v8-$V8_VERSION-$J2V8_VERSION-mac-x86.dynlib
+cp out/Release/libj2v8.dynlib lib/libj2v8-$V8_VERSION-$J2V8_VERSION-mac-x86.dynlib
 
 make clean
 ./configure --dest-cpu=x64
 make -j8
-cp out/Release/lib.target/libj2v8.dynlib lib/libj2v8-$V8_VERSION-$J2V8_VERSION-mac-x64.dynlib
+cp out/Release/libj2v8.dynlib lib/libj2v8-$V8_VERSION-$J2V8_VERSION-mac-x64.dynlib
