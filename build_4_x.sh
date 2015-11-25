0208d0a0e728e6c6bd81e94e68d7594cbe9c9b7e
Ian Bull <irbull@eclipsesource.com>
2015-11-25 11:46:37 +0100

    Update the V8 4.x build to a much cleaner impl.
    Thanks to Daniel Boelzle for this script.

--- build_4_x.sh
+++ build_4_x.sh
@@ -1,6 +1,25 @@
+export JAVA_HOME=$(/usr/libexec/java_home)
+V8=../../v8
+V8_BUILD=$V8/out/x64.release
+
 cd target/classes
 javah com.eclipsesource.v8.V8
 cp com_eclipsesource_v8_V8.h ../../jni/com_eclipsesource_v8_V8Impl.h
 cd ../../jni
-clang++  -stdlib=libstdc++ -Iinclude ../v8_bleeding_edge/libv8_libbase.a ../v8_bleeding_edge/libv8_libplatform.a ../v8_bleeding_edge/libv8_base.a ../v8_bleeding_edge/libv8_nosnapshot.a ../v8_bleeding_edge/libicudata.a ../v8_bleeding_edge/libicuuc.a ../v8_bleeding_edge/libicui18n.a -I ../v8_bleeding_edge -I /Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home/include -I /Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home/include/darwin/ -shared -o libj2v8_macosx_x86_64.dylib com_eclipsesource_v8_V8Impl.cpp  -std=c++11
 
+clang++  -stdlib=libstdc++ \
+        -Iinclude $V8_BUILD/libv8_libbase.a \
+        $V8_BUILD/libv8_libplatform.a \
+        $V8_BUILD/libv8_base.a \
+        $V8_BUILD/libv8_nosnapshot.a \
+        $V8_BUILD/libicudata.a \
+        $V8_BUILD/libicuuc.a \
+        $V8_BUILD/libicui18n.a \
+        -I $V8 \
+        -I $JAVA_HOME/include \
+        -I $JAVA_HOME/include/darwin/ \
+        -shared -o libj2v8_macosx_x86_64.dylib \
+        com_eclipsesource_v8_V8Impl.cpp \
+        -std=c++11
+
+echo fini
