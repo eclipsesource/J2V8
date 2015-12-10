7d8da7a99d1873b027734f49702a3c6e9ac49a28
Ian Bull <irbull@eclipsesource.com>
2015-12-10 11:29:11 -0800

    Fix deployment issues and assemble bundles for creating p2 repo
    There were a few issues with deployment
     - The pom files didn't properly contains the WS
     - Bundles were not being created
     - The bundles were not being saved to create a p2 repo
    
    This change-set fixes those issues

--- deploy_snapshots.sh
+++ deploy_snapshots.sh
@@ -7,10 +7,12 @@ echo "Deploying MacOS"
 rm src/main/resources/*j2v8*
 cp jni/libj2v8_macosx_x86_64.dylib src/main/resources/libj2v8_macosx_x86_64.dylib
 sed s/\$\{os\}/macosx/g < pom_template.xml  > pom1.xml
-sed s/\$\{arch\}/x86_64/g < pom1.xml  > pom.xml
+sed s/\$\{arch\}/x86_64/g < pom1.xml  > pom2.xml
+sed s/\$\{ws\}/cocoa/g < pom2.xml  > pom.xml
 mvn -Dos=macosx -Darch=x86_64 clean deploy $SUFFIX
 cp pom_template.xml pom.xml
 rm pom1.xml
+rm pom2.xml
 STATUS=$?
 if [ $STATUS -eq 0 ]; then
  echo "MacOS Deployment Successful"
@@ -18,15 +20,19 @@ else
  echo "MacOS Deployment Failed"
  exit $STATUS
 fi
+cp target/j2v8_macosx_x86_64-*.jar releng/plugins
 
 echo "Deploying Win32/x86"
 rm src/main/resources/*j2v8*
 cp jni/libj2v8_win32_x86.dll src/main/resources/libj2v8_win32_x86.dll
 sed s/\$\{os\}/win32/g < pom_template.xml  > pom1.xml
-sed s/\$\{arch\}/x86/g < pom1.xml  > pom.xml
+sed s/\$\{arch\}/x86/g < pom1.xml  > pom2.xml
+sed s/\$\{ws\}/win32/g < pom2.xml  > pom.xml
+mvn -Dos=macosx -Darch=x86_64 clean deploy $SUFFIX
 mvn -Dos=win32 -Darch=x86 clean deploy $SUFFIX
 cp pom_template.xml pom.xml
 rm pom1.xml
+rm pom2.xml
 STATUS=$?
 if [ $STATUS -eq 0 ]; then
  echo "Win32 Deployment Successful"
@@ -34,15 +40,18 @@ else
  echo "Win32 Deployment Failed"
  exit $STATUS
 fi
+cp target/j2v8_win32_x86-*.jar releng/plugins
 
 echo "Deploying Win32/x64"
 rm src/main/resources/*j2v8*
 cp jni/libj2v8_win32_x86_64.dll src/main/resources/libj2v8_win32_x86_64.dll
 sed s/\$\{os\}/win32/g < pom_template.xml  > pom1.xml
-sed s/\$\{arch\}/x86_64/g < pom1.xml  > pom.xml
+sed s/\$\{arch\}/x86_64/g < pom1.xml  > pom2.xml
+sed s/\$\{ws\}/win32/g < pom2.xml  > pom.xml
 mvn -Dos=win32 -Darch=x86 clean deploy $SUFFIX
 cp pom_template.xml pom.xml
 rm pom1.xml
+rm pom2.xml
 STATUS=$?
 if [ $STATUS -eq 0 ]; then
  echo "Win32_64 Deployment Successful"
@@ -50,15 +59,18 @@ else
  echo "Win32_64 Deployment Failed"
  exit $STATUS
 fi
+cp target/j2v8_win32_x86_64-*.jar releng/plugins
 
 echo "Deploying Linux"
 rm src/main/resources/*j2v8*
 cp jni/libj2v8_linux_x86_64.so src/main/resources/libj2v8_linux_x86_64.so
 sed s/\$\{os\}/linux/g < pom_template.xml  > pom1.xml
-sed s/\$\{arch\}/x86_64/g < pom1.xml  > pom.xml
+sed s/\$\{arch\}/x86_64/g < pom1.xml  > pom2.xml
+sed s/\$\{ws\}/gtk/g < pom1.xml  > pom.xml
 mvn -Dos=linux -Darch=x86_64 clean deploy $SUFFIX
 cp pom_template.xml pom.xml
 rm pom1.xml
+rm pom2.xml
 STATUS=$?
 if [ $STATUS -eq 0 ]; then
  echo "Linux Deployment Successful"
@@ -66,6 +78,7 @@ else
  echo "Linux Deployment Failed"
  exit $STATUS
 fi
+cp target/j2v8_linux_x86_64-*.jar releng/plugins
 
 rm src/main/resources/*j2v8*
 
