dc73f8d9bd7027e22e26649bb78ea5a64e17cb5e
Brandon Sanders <brandon@alicorn.io>
2016-09-27 10:26:25 -0400

    Fix two stray headers.

--- src/main/java/com/eclipsesource/v8/ConcurrentV8.java
+++ src/main/java/com/eclipsesource/v8/ConcurrentV8.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2016 Alicorn Systems
+ * Copyright (c) 2016 Brandon Sanders
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Alicorn Systems - initial API and implementation and/or initial documentation
+ *    Brandon Sanders - initial API and implementation and/or initial documentation
  ******************************************************************************/
 package com.eclipsesource.v8;
 
