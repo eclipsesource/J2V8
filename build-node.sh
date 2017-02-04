d2f2b009f52c63e91070275c4915271208100c29
KnownUnown <andrew.tianyi.pan@gmail.com>
2017-02-04 01:00:43 -0600

    Patch J2V8 for Node 7.4.0

--- build-node.sh
+++ build-node.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/nodejs/node.git
 cd node
-git checkout v6.1.0
+git checkout v7.4.0
 ./configure --enable-static
 make
 cd ..
--- jni/Android.mk
+++ jni/Android.mk
@@ -1,63 +1,114 @@
 LOCAL_PATH := $(call my-dir)
 LOCAL_MULTILIB := "both"
+TARGET_PATH := $(LOCAL_PATH)/../node/out/Release/obj.target
+
+# node
 
 include $(CLEAR_VARS)
-LOCAL_MODULE	:= v8_base
-#LOCAL_SRC_FILES := ../support/android/libs/libv8_base.arm.a
-#LOCAL_SRC_FILES := ../libv8_base.arm.a
-#LOCAL_SRC_FILES := /data/jenkins/v8_3_26/out/android_$(TARGET_ARCH_ABI).release/obj.target/tools/gyp/libv8_base.$(TARGET_ARCH_ABI).a
-LOCAL_SRC_FILES := /data/jenkins/v8/out/android_$(TARGET_ARCH_ABI).release/obj.target/tools/gyp/libv8_base.a
+LOCAL_MODULE	:= node
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/libnode.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+# deps
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= uv
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/deps/uv/libuv.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE	:= openssl
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/deps/openssl/libopenssl.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= http_parser
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/deps/http_parser/libhttp_parser.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= gtest
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/deps/gtest/libgtest.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= zlib
+LOCAL_SRC_FILES := $(TARGET_PATH)/deps/zlib/libzlib.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= cares
+LOCAL_SRC_FILES := $(TARGET_PATH)/deps/cares/libcares.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+# intl
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= icuucx
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/tools/icu/libicuucx.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= icui18n
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/tools/icu/libicui18n.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= icudata
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/tools/icu/libicudata.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= icustubdata
+LOCAL_SRC_FILES := $(TARGET_PATH)/tools/icu/libicustubdata.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+# v8
+
+include $(CLEAR_VARS)
 LOCAL_MODULE	:= v8_libbase
-#LOCAL_SRC_FILES := ../support/android/libs/libv8_base.arm.a
-#LOCAL_SRC_FILES := ../libv8_base.arm.a
-#LOCAL_SRC_FILES := /data/jenkins/v8_3_26/out/android_$(TARGET_ARCH_ABI).release/obj.target/tools/gyp/libv8_base.$(TARGET_ARCH_ABI).a
-LOCAL_SRC_FILES := /data/jenkins/v8/out/android_$(TARGET_ARCH_ABI).release/obj.target/tools/gyp/libv8_libbase.a
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/deps/v8/src/libv8_libbase.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= v8_base
+LOCAL_SRC_FILES := $(TARGET_PATH)/deps/v8/src/libv8_base.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE	:= v8_libplatform
-#LOCAL_SRC_FILES := ../support/android/libs/libv8_base.arm.a
-#LOCAL_SRC_FILES := ../libv8_base.arm.a
-#LOCAL_SRC_FILES := /data/jenkins/v8_3_26/out/android_$(TARGET_ARCH_ABI).release/obj.target/tools/gyp/libv8_base.$(TARGET_ARCH_ABI).a
-LOCAL_SRC_FILES := /data/jenkins/v8/out/android_$(TARGET_ARCH_ABI).release/obj.target/tools/gyp/libv8_libplatform.a
+LOCAL_SRC_FILES := $(TARGET_PATH)/deps/v8/src/libv8_libplatform.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE	:= v8_nosnapshot
-#LOCAL_SRC_FILES :=  ../support/android/libs/libv8_nosnapshot.arm.a
-#LOCAL_SRC_FILES :=  ../libv8_nosnapshot.arm.a
-LOCAL_SRC_FILES :=  /data/jenkins/v8/out/android_$(TARGET_ARCH_ABI).release/obj.target/tools/gyp/libv8_nosnapshot.a
+LOCAL_SRC_FILES := $(TARGET_PATH)/deps/v8/src/libv8_nosnapshot.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE	:= libicuuc
-#LOCAL_SRC_FILES :=  ../support/android/libs/libicuuc.a
-#LOCAL_SRC_FILES :=  ../libicuuc.a
-#LOCAL_SRC_FILES :=  /data/jenkins/v8_3_26/out/android_arm.release/obj.target/tools/gyp/libicuuc.a
-#include $(PREBUILT_STATIC_LIBRARY)
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE	:= libicui18n
-#LOCAL_SRC_FILES :=  ../support/android/libs/libicui18n.a
-#LOCAL_SRC_FILES :=  ../libicui18n.a
-#include $(PREBUILT_STATIC_LIBRARY)
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE	:= libicudata
-#LOCAL_SRC_FILES :=  ../support/android/libs/libicudata.a
-#LOCAL_SRC_FILES :=  ../libicudata.a
-#include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VAR)
+LOCAL_MODULE	:= v8_libsampler
+LOCAL_SRC_FILES := $(TARGET_PATH)/deps/v8/src/libv8_libsampler.a
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := j2v8_android_$(TARGET_ARCH_ABI)
+LOCAL_MODULE	:= v8_libstandalone_inspector
+LOCAL_SRC_FILES	:= $(TARGET_PATH)/deps/v8_inspector/src/inspector/libstandalone_inspector.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := j2v8
+TARGET_OUT		:= jniLibs/$(TARGET_ARCH_ABI)
+
 LOCAL_SRC_FILES := com_eclipsesource_v8_V8Impl.cpp
-#LOCAL_C_INCLUDES := /data/jenkins/v8_3_26/include/
-LOCAL_C_INCLUDES := /data/jenkins/v8/include /data/jenkins/v8
-LOCAL_CFLAGS += -std=c++11 -Wall -Wno-unused-function -Wno-unused-variable -O3 -funroll-loops -ftree-vectorize -ffast-math -fpermissive -fpic
-#LOCAL_STATIC_LIBRARIES := v8_base v8_nosnapshot libicui18n libicuuc libicudata
-LOCAL_STATIC_LIBRARIES := v8_base v8_libbase v8_libplatform v8_nosnapshot
-LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog -latomic
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../node $(LOCAL_PATH)/../node/deps/v8 $(LOCAL_PATH)/../node/deps/v8/include $(LOCAL_PATH)/../node/deps/icu-small/source $(LOCAL_PATH)/../node/src
+LOCAL_CFLAGS	+= -std=c++11 -Wall -Wno-unused-function -Wno-unused-variable -O3 -funroll-loops -ftree-vectorize -ffast-math -fpermissive -fPIC -D NODE_COMPATIBLE=1
+LOCAL_LDLIBS	+= -L$(SYSROOT)/usr/lib -llog -latomic
+
+LOCAL_WHOLE_STATIC_LIBRARIES := node
+LOCAL_STATIC_LIBRARIES := \
+	uv openssl http_parser gtest zlib cares \
+	v8_base v8_nosnapshot v8_libplatform v8_libbase v8_libsampler v8_libstandalone_inspector \
+	icui18n icuucx icudata icustubdata
+
 include $(BUILD_SHARED_LIBRARY)
