7106753ea7de978ec8a75165b907bbb03f42ed22
Ian Bull <irbull@eclipsesource.com>
2017-04-29 22:37:43 -0700

    Add platform independent builds
    This adds platform independent builds for Linux and
    Android. These builds will run on any machine that
    has Docker installed. This assumes a target platform
    to compile against.
    
    The target platform is a pre-built node.js and can
    be fetched from [1].
    
    [1] https://github.com/eclipsesource/static-node-build
    
    Change-Id: I991e7db25d2242a014b0f50bb56614f27a9f9c7a

---
+++ buildAll.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+DIR=`dirname $0`
+
+set +e
+rm -rf node.out
+docker rm -f j2v8.android.x86
+docker rm -f j2v8.android.arm
+docker rm -f j2v8.linux.x64
+set -e
+
+tar xvzf node.out-7_4_0.tar.gz
+
+docker build -t "j2v8-android-x86" -f docker/Dockerfile.android $DIR
+docker run -v $PWD/node.out:/build/node --name j2v8.android.x86 j2v8-android-x86 android-gcc-toolchain x86 --api 15 --host gcc-lpthread -C sh -c "cd jni && ndk-build" 
+docker cp j2v8.android.x86:/build/jni/jniLibs $DIR/src/main/
+
+docker build -t "j2v8-linux-x64" -f docker/Dockerfile.linux $DIR
+docker run -v $PWD/node.out:/build/node --name j2v8.linux.x64 j2v8-linux-x64 
+#docker cp j2v8.linux.x64:/build/jni/jniLibs $DIR/src/main/
+
