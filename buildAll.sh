810ebe13d67364ef2bfc86caef4f6f135f98f1ae
Ian Bull <irbull@eclipsesource.com>
2017-05-03 15:07:27 -0700

    Build on linux first
    Since linux is where we run the tests, build on linux first
    so we fast-fail if things go wrong.
    
    Change-Id: I643aaa2416d2f4820a651c2699b5ce110e2a09f7

--- buildAll.sh
+++ buildAll.sh
@@ -12,11 +12,11 @@ set -e
 
 tar xzf node.out-7_4_0.tar.gz
 
+docker build -t "j2v8-linux-x64" -f docker/Dockerfile.linux $DIR
+docker run -v $PWD:/build/. -v $PWD/node.out:/build/node --name j2v8.linux.x64 j2v8-linux-x64 
+
 docker build -t "j2v8-android-x86" -f docker/Dockerfile.android $DIR
 docker run -v $PWD/node.out:/build/node --name j2v8.android.x86 j2v8-android-x86 android-gcc-toolchain x86 --api 15 --host gcc-lpthread -C sh -c "cd jni && ndk-build" 
 docker cp j2v8.android.x86:/build/jni/jniLibs $DIR/src/main/
 
-docker build -t "j2v8-linux-x64" -f docker/Dockerfile.linux $DIR
-docker run -v $PWD:/build/. -v $PWD/node.out:/build/node --name j2v8.linux.x64 j2v8-linux-x64 
-#docker cp j2v8.linux.x64:/build/jni/jniLibs $DIR/src/main/
 
