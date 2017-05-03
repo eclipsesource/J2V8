bb8185317410667cb555c5cdcec9f515c610eabd
Ian Bull <irbull@eclipsesource.com>
2017-05-02 17:09:00 -0700

    Don't print the tar extraction
    The tar extraction is producing too much log output. This
    removes it from the build script.
    
    Change-Id: Icb46719e0d5caf65aeb7aecf58af715cb30abde2

--- buildAll.sh
+++ buildAll.sh
@@ -10,7 +10,7 @@ docker rm -f j2v8.android.arm
 docker rm -f j2v8.linux.x64
 set -e
 
-tar xvzf node.out-7_4_0.tar.gz
+tar xzf node.out-7_4_0.tar.gz
 
 docker build -t "j2v8-android-x86" -f docker/Dockerfile.android $DIR
 docker run -v $PWD/node.out:/build/node --name j2v8.android.x86 j2v8-android-x86 android-gcc-toolchain x86 --api 15 --host gcc-lpthread -C sh -c "cd jni && ndk-build" 
