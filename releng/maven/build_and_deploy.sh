7ae6fe0ef64dfa94d000b284d1cba9ab0691d033
Ian Bull <irbull@eclipsesource.com>
2017-05-05 11:51:02 -0700

    Add the Git ID during the build to getSCMRevision
    During a build and deploy, add the Git ID to the getSCMRevision
    API. This way, you can get the actual J2V8 Source Revision
    you are running.
    
    Change-Id: I5f3907643335b0d0e45babf3beef1c44cb68c7ec

--- releng/maven/build_and_deploy.sh
+++ releng/maven/build_and_deploy.sh
@@ -3,6 +3,13 @@ set -e
 rm -rf j2v8
 git clone https://github.com/eclipsesource/j2v8
 cd j2v8
+
+### Add the commit ID for the latest commit to V8
+COMMIT_ID=`git rev-parse HEAD`
+sed s/Unknown\ revision\ ID/$COMMIT_ID/ ./src/main/java/com/eclipsesource/v8/V8.java > ./src/main/java/com/eclipsesource/v8/V8.java.updated
+mv ./src/main/java/com/eclipsesource/v8/V8.java.updated ./src/main/java/com/eclipsesource/v8/V8.java
+exit 0;
+
 TARGET_PLATFORM=$PWD/../../../node.out-7_4_0.tar.gz
 if [ -f $TARGET_PLATFORM ]; then
   cp $TARGET_PLATFORM .
