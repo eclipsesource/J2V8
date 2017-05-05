223a889a8bbacb6887732b0631a35d1bd2fd07b7
Ian Bull <irbull@eclipsesource.com>
2017-05-05 11:59:46 -0700

    Fix the build and deploy script
    Accidently left in the exit statement we used when
    debugging.
    
    Change-Id: I4ea0ce0e610948b9c571e72f1fc9adc2e4e4ce54

--- releng/maven/build_and_deploy.sh
+++ releng/maven/build_and_deploy.sh
@@ -8,7 +8,6 @@ cd j2v8
 COMMIT_ID=`git rev-parse HEAD`
 sed s/Unknown\ revision\ ID/$COMMIT_ID/ ./src/main/java/com/eclipsesource/v8/V8.java > ./src/main/java/com/eclipsesource/v8/V8.java.updated
 mv ./src/main/java/com/eclipsesource/v8/V8.java.updated ./src/main/java/com/eclipsesource/v8/V8.java
-exit 0;
 
 TARGET_PLATFORM=$PWD/../../../node.out-7_4_0.tar.gz
 if [ -f $TARGET_PLATFORM ]; then
