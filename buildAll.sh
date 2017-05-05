d5a407a39b47cf1085fa51ae60dc34dab4380326
Ian Bull <irbull@eclipsesource.com>
2017-05-04 18:32:21 -0700

    Strip symbols once we build the android binaries
    Once the android binaries are built, strip the symbols
    from the shared object files.
    
    Change-Id: Idee2f733a40fa1524e90dd6a5de0e9e794047afd

--- buildAll.sh
+++ buildAll.sh
@@ -16,7 +16,7 @@ docker build -t "j2v8-linux-x64" -f docker/Dockerfile.linux $DIR
 docker run -v $PWD:/build/. -v $PWD/node.out:/build/node --name j2v8.linux.x64 j2v8-linux-x64 
 
 docker build -t "j2v8-android-x86" -f docker/Dockerfile.android $DIR
-docker run -v $PWD/node.out:/build/node --name j2v8.android.x86 j2v8-android-x86 android-gcc-toolchain x86 --api 15 --host gcc-lpthread -C sh -c "cd jni && ndk-build" 
+docker run -v $PWD/node.out:/build/node --name j2v8.android.x86 j2v8-android-x86 android-gcc-toolchain x86 --api 15 --host gcc-lpthread -C sh -c "cd jni && ndk-build && /build/android-ndk-r13b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/strip --strip-unneeded -R .note -R .comment /build/jni/jniLibs/armeabi-v7a/libj2v8.so && strip --strip-unneeded -R .note -R .comment /build/jni/jniLibs/x86/libj2v8.so"
 docker cp j2v8.android.x86:/build/jni/jniLibs $DIR/src/main/
 
 
