02314a4b5f3641bb1de7e643f5e0b5bfadb51a56
Ian Bull <irbull@eclipsesource.com>
2017-05-09 10:34:17 -0700

    Correct a typo in the build script
    The build script had a double $$. This fixes that.
    
    Change-Id: I4b3b2c0084081663b78a83ec178faeebdb854a38

--- buildAll.sh
+++ buildAll.sh
@@ -41,7 +41,7 @@ set -e
 tar xzf node.out-7_4_0.tar.gz
 
 docker build -t "j2v8-linux-x64" -f docker/Dockerfile.linux $DIR
-docker run -v $ROOTPATH:/build/. -v $$ROOTPATH/node.out:/build/node --name j2v8.linux.x64 j2v8-linux-x64 
+docker run -v $ROOTPATH:/build/. -v $ROOTPATH/node.out:/build/node --name j2v8.linux.x64 j2v8-linux-x64 
 
 docker build -t "j2v8-android-x86" -f docker/Dockerfile.android $DIR
 docker run -v $ROOTPATH/node.out:/build/node --name j2v8.android.x86 j2v8-android-x86 android-gcc-toolchain x86 --api 15 --host gcc-lpthread -C sh -c "cd jni && ndk-build && /build/android-ndk-r13b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/strip --strip-unneeded -R .note -R .comment /build/jni/jniLibs/armeabi-v7a/libj2v8.so && strip --strip-unneeded -R .note -R .comment /build/jni/jniLibs/x86/libj2v8.so"
