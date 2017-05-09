9f3e634e7e7e290586fd357b7f8036c08c334c04
Ian Bull <irbull@eclipsesource.com>
2017-05-09 16:09:14 -0700

    Update the local build scripts I use while developing
    Change-Id: Iefb0a6075da3c6d7a5c48501797b9b31b23b5e0a

---
+++ build-node.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/nodejs/node -b v7.4.0
+cd node
+./configure --enable-static
+make
--- build_node.sh
+++
@@ -1,6 +0,0 @@
-cd target/classes
-javah com.eclipsesource.v8.V8
-cp com_eclipsesource_v8_V8.h ../../jni/com_eclipsesource_v8_V8Impl.h
-cd ../../jni
-clang++  -stdlib=libstdc++ -Iinclude -I../node/include -I../node -Wl,-force_load,../node/libnode.a ../node/libv8_libbase.a ../node/libv8_libplatform.a ../node/libv8_base.a ../node/libv8_nosnapshot.a ../node/libuv.a ../node/libopenssl.a ../node/libhttp_parser.a ../node/libgtest.a ../node/libzlib.a ../node/libcares.a -I../v8_bleeding_edge -I /Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home/include -I /Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home/include/darwin/ -shared -o libj2v8_macosx_x86_64.dylib com_eclipsesource_v8_V8Impl.cpp  -std=c++11 -Wwritable-strings
-
