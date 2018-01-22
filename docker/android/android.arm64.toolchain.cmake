217f06eb737ac01a947b33dec6282c2f30a576ff
Johan Halin <johan@richie.fi>
2018-01-22 12:49:23 +0200

    Added ARM64 CMake file.

---
+++ docker/android/android.arm64.toolchain.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME Android)
+set(CMAKE_SYSTEM_VERSION 21) # API level
+
+set(CMAKE_ANDROID_ARCH arm)
+set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
+set(CMAKE_ANDROID_NDK /build/android-ndk-r13b/)
+set(CMAKE_ANDROID_STL_TYPE gnustl_static)
