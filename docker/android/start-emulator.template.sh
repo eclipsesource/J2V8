a3fabba2e43597f8f4b564d3688f8996cc1ba7cc
Ian Bull <irbull@eclipsesource.com>
2019-01-06 17:38:31 -0800

    Create a gitlab ci build for J2V8
    Add a gitlab-ci yaml file so J2V8 can be built on GitLab.
    The gitlabci uses a base docker-dind image, and adds the
    required dependencies. It also sets the commit hash
    in the V8 method that returns the SCM ID. This is useful
    when debugging.
    
    In addition to the new build, this fixes a typo in the name
    of the emulator. Since we updated to a newer NDK build, the emulator
    was suffixed with '64'.
    
    Change-Id: I60b420d16ca26f06a4046b9793e31c47de5220cd

--- docker/android/start-emulator.template.sh
+++ docker/android/start-emulator.template.sh
@@ -1,2 +1,2 @@
 echo no | $ANDROID_HOME/tools/android create avd -f -n test -k "system-images;android-19;default;armeabi-v7a"
-echo no | $ANDROID_HOME/emulator/emulator64$EMU_ARCH -avd test -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet -vnc :0
+echo no | $ANDROID_HOME/emulator/emulator$EMU_ARCH -avd test -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet -vnc :0
