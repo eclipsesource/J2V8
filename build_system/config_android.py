1aadfc1cd38530d1315bd498d6002d9cbd1ab4ab
Ian Bull <irbull@eclipsesource.com>
2019-01-10 13:22:51 -0800

    Support 64-bit Android builds
    Adds support for both x86_64 and arm64 builds for J2V8. This sets
    the API version to 23, as version 17 doesn't support 64-bit builds.
    
    This change-set also updates the gitlab-ci build to produce all
    four platforms in a single aar file.
    
    Change-Id: Ie5abc5593a0acc252a1a02f2372af23fae499688

--- build_system/config_android.py
+++ build_system/config_android.py
@@ -6,7 +6,7 @@ import build_utils as b
 import cmake_utils as cmu
 import os
 
-android_config = PlatformConfig(c.target_android, [c.arch_x86, c.arch_arm])
+android_config = PlatformConfig(c.target_android, [c.arch_x86, c.arch_arm, c.arch_arm64, c.arch_x86_64])
 
 android_config.set_cross_configs({
     "docker": DockerBuildStep(
@@ -22,21 +22,28 @@ android_config.set_cross_compilers({
 
 android_config.set_file_abis({
     c.arch_arm: "armeabi-v7a",
-    c.arch_x86: "x86"
+    c.arch_x86: "x86",
+    c.arch_x86_64: "x86_64",
+    c.arch_arm64: "arm64-v8a"
 })
 
 #-----------------------------------------------------------------------
 def build_node_js(config):
+    arch = config.inject_env("$ARCH")
+    if ("x86_64" in arch):
+        os.environ['DEST_CPU'] = "x64"
+    else:
+        os.environ['DEST_CPU'] = arch
     return [
-        """android-gcc-toolchain $ARCH --api 17 --host gcc-lpthread -C \
+        """android-gcc-toolchain $ARCH --api 23 --host gcc-lpthread -C \
             sh -c \"                \\
             cd ./node;              \\
             ./configure             \\
             --cross-compiling       \\
-            --without-intl          \\
             --without-inspector     \\
-            --dest-cpu=$ARCH        \\
+            --dest-cpu=$DEST_CPU    \\
             --dest-os=$PLATFORM     \\
+            --openssl-no-asm        \\
             --without-snapshot      \\
             --enable-static &&      \\
             CFLAGS=-fPIC CXXFLAGS=-fPIC make -j4\"  \
--- docker/android/android.arm64.toolchain.cmake
+++ docker/android/android.arm64.toolchain.cmake
@@ -1,10 +1,8 @@
 set(CMAKE_SYSTEM_NAME Android)
 set(CMAKE_SYSTEM_VERSION 23) # API level
 
-set(CMAKE_ANDROID_ARCH arm)
-set(CMAKE_ANDROID_ARCH_ABI armeabi-v7a)
+set(CMAKE_ANDROID_ARCH arm64)
+set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
 set(CMAKE_ANDROID_NDK /build/android-ndk-r14b/)
 set(CMAKE_ANDROID_STL_TYPE gnustl_static)
 
-# ARM specific settings
-set(CMAKE_ANDROID_ARM_NEON 1)
