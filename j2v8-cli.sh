123331f74bbb46636499186c8dbc384009fa118f
Elmi Ahmadov <eahmadov@eclipsesource.com>
2019-01-18 11:04:01 +0100

    Add missing requirements and improve bash script
    * Add requirements section to build guideline.
    * Since `Python 2` is required to build, bash script is improved to
    verify python2 is installed.
    * Fix interactive build arguments typo in Build-System CLI section.

--- j2v8-cli.sh
+++ j2v8-cli.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 # This script adds aliases for some of the most often used commands for building J2V8
 # to your current command-shell instance. (can be invoked as "source j2v8-cli.sh")
-alias build="python build.py"
-alias nodejs="python nodejs.py"
-alias citests="python build_system/run_tests.py"
+
+if command -v python2 &>/dev/null; then
+	alias build="python2 build.py"
+	alias nodejs="python2 nodejs.py"
+	alias citests="python2 build_system/run_tests.py"
+else
+	 echo Python 2 is not installed
+fi
